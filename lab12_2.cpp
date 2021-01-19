bb3232793b5d3db47cb5ac59bdb10dff5a444859
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-01-19 16:52:05 +0700

    lab12_2 finally done!!

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,25 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool map[][M]){ ;
+    for(int i=0;i<N+2;i++){
+        for(int j=0;j<M+2;j++){
+            if(i==0||i==N+1)cout<<"-";
+            else if(j==0||j==M+1)cout<<"|";
+            if(i>0&&i<N+1&&j>0&&j<M+1){
+                if(map[i-1][j-1]==0)cout<<" ";
+                else if(map[i-1][j-1]==1)cout<<"*";
+                }
+        }
+        cout<<endl;
+    }
+}
+
+void updateImage(bool map[][M], int s, int x, int y){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            if(sqrt(pow(i-x,2)+pow(j-y,2))<=s-1){map[i][j]=1;}
+        }
+    }
+}
